ed233893c324b21cfc9969cf05d3783efce935e4
KonunKMUTT <156557056+KonunKMUTT@users.noreply.github.com>
2024-02-11 22:04:59 +0700

    Main_app.py

--- Main_app.py
+++ Main_app.py
@@ -7,8 +7,6 @@ conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 ext_data = conn.read(wroksheet="Sheet1", usecols=list(range(10)), ttl=5)
 ext_data = ext_data.dropna(how="all")
 
-st.dataframe(ext_data)
-
 # Load the model
 with open('model.pkl', 'rb') as file:
     model = pickle.load(file)
