ff016b7f897a1ca1e67aead4faec1e8b2b1a5032
KonunKMUTT <156557056+KonunKMUTT@users.noreply.github.com>
2024-02-11 22:39:37 +0700

    Main_app.py

--- Main_app.py
+++ Main_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
-ext_data = conn.read(wroksheet="Sheet1", usecols=list(range(10)), ttl=5)
+ext_data = conn.read(worksheet="Sheet1", usecols=list(range(10)), ttl=5)
 ext_data = ext_data.dropna(how="all")
 
 # Load the model
@@ -28,6 +28,17 @@ def predict_heart_disease(age, impulse, pressure_high, pressure_low, glucose, kc
     y_pred_new = model.predict(x_new)
     return y_pred_new[0].strip()
 
+# Function to clear input fields
+def clear_input_fields():
+    st.text_input("Enter age:", value="")
+    st.text_input("Enter impulse:", value="")
+    st.text_input("Enter high blood pressure:", value="")
+    st.text_input("Enter low blood pressure:", value="")
+    st.text_input("Enter glucose level:", value="")
+    st.text_input("Enter KCM:", value="")
+    st.text_input("Enter troponin level:", value="")
+    st.selectbox("Select gender", ["Female", "Male"], index=0)
+
 # Streamlit app
 def main():
     st.markdown('<p style="text-align:center; font-weight:bold; font-size:30px;">Heart Disease Prediction App</p>', unsafe_allow_html=True)
@@ -48,7 +59,7 @@ def main():
     else:
         female = 0
         male = 1
-           
+
     if st.button("Predict"):
         result = predict_heart_disease(age, impulse, pressure_high, pressure_low, glucose, kcm, troponin, female, male)
         # Set color based on the result
@@ -59,23 +70,26 @@ def main():
         st.markdown(styled_result, unsafe_allow_html=True)
 
         new_data = pd.DataFrame({
-        'age': [age],
-        'impluse': [impulse],
-        'pressurehight': [pressure_high],
-        'pressurelow': [pressure_low],
-        'glucose': [glucose],
-        'kcm': [kcm],
-        'troponin': [troponin],
-        'female': [female],
-        'male': [male],
-        'class' : [result]
-        })
+            'age': [age],
+            'impluse': [impulse],
+            'pressurehight': [pressure_high],
+            'pressurelow': [pressure_low],
+            'glucose': [glucose],
+            'kcm': [kcm],
+            'troponin': [troponin],
+            'female': [female],
+            'male': [male],
+            'class' : [result]
+            })
 
         update_df = pd.concat([ext_data, new_data], ignore_index=True)
         conn.update(worksheet="Sheet1", data=update_df)
 
         st.success("New Data is Update To GoogleSheets!")
-        st.stop()
+
+    # Clear button functionality
+    if st.button("Clear"):
+        clear_input_fields()
 
 if __name__ == '__main__':
     main()
