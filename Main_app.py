66dc0a63cd87d0291abff175e717301580040fd7
KonunKMUTT <156557056+KonunKMUTT@users.noreply.github.com>
2024-02-11 22:40:51 +0700

    Main_app.py

--- Main_app.py
+++ Main_app.py
@@ -28,32 +28,21 @@ def predict_heart_disease(age, impulse, pressure_high, pressure_low, glucose, kc
     y_pred_new = model.predict(x_new)
     return y_pred_new[0].strip()
 
-# Function to clear input fields
-def clear_input_fields():
-    st.text_input("Enter age:", value="")
-    st.text_input("Enter impulse:", value="")
-    st.text_input("Enter high blood pressure:", value="")
-    st.text_input("Enter low blood pressure:", value="")
-    st.text_input("Enter glucose level:", value="")
-    st.text_input("Enter KCM:", value="")
-    st.text_input("Enter troponin level:", value="")
-    st.selectbox("Select gender", ["Female", "Male"], index=0)
-
 # Streamlit app
 def main():
     st.markdown('<p style="text-align:center; font-weight:bold; font-size:30px;">Heart Disease Prediction App</p>', unsafe_allow_html=True)
 
-    age = st.text_input("Enter age:")
-    impulse = st.text_input("Enter impulse:")
-    pressure_high = st.text_input("Enter high blood pressure:")
-    pressure_low = st.text_input("Enter low blood pressure:")
-    glucose = st.text_input("Enter glucose level:")
-    kcm = st.text_input("Enter KCM:")
-    troponin = st.text_input("Enter troponin level:")
-    gender = st.selectbox("Select gender", ["Female", "Male"])
+    # Initialize input variables
+    age_input = st.text_input("Enter age:")
+    impulse_input = st.text_input("Enter impulse:")
+    pressure_high_input = st.text_input("Enter high blood pressure:")
+    pressure_low_input = st.text_input("Enter low blood pressure:")
+    glucose_input = st.text_input("Enter glucose level:")
+    kcm_input = st.text_input("Enter KCM:")
+    troponin_input = st.text_input("Enter troponin level:")
+    gender_input = st.selectbox("Select gender", ["Female", "Male"])
 
-    # Set gender value based on selection
-    if gender == "Female":
+    if gender_input == "Female":
         female = 1
         male = 0
     else:
@@ -61,22 +50,19 @@ def main():
         male = 1
 
     if st.button("Predict"):
-        result = predict_heart_disease(age, impulse, pressure_high, pressure_low, glucose, kcm, troponin, female, male)
-        # Set color based on the result
-        color = "red" if result == "positive" else "green"  # Adjust this condition based on your model's output
-        # Apply styling with HTML
+        result = predict_heart_disease(age_input, impulse_input, pressure_high_input, pressure_low_input, glucose_input, kcm_input, troponin_input, female, male)
+        color = "red" if result == "positive" else "green"
         styled_result = f'<p style="color:{color}; font-size:20px; text-align:center; font-weight:bold; background-color:#4B4A54; padding:10px; border-radius: 15px;">{result}</p>'
-        # Display the styled result
         st.markdown(styled_result, unsafe_allow_html=True)
 
         new_data = pd.DataFrame({
-            'age': [age],
-            'impluse': [impulse],
-            'pressurehight': [pressure_high],
-            'pressurelow': [pressure_low],
-            'glucose': [glucose],
-            'kcm': [kcm],
-            'troponin': [troponin],
+            'age': [age_input],
+            'impluse': [impulse_input],
+            'pressurehight': [pressure_high_input],
+            'pressurelow': [pressure_low_input],
+            'glucose': [glucose_input],
+            'kcm': [kcm_input],
+            'troponin': [troponin_input],
             'female': [female],
             'male': [male],
             'class' : [result]
@@ -89,7 +75,15 @@ def main():
 
     # Clear button functionality
     if st.button("Clear"):
-        clear_input_fields()
+        # Update input variables with empty strings
+        age_input = ""
+        impulse_input = ""
+        pressure_high_input = ""
+        pressure_low_input = ""
+        glucose_input = ""
+        kcm_input = ""
+        troponin_input = ""
+        gender_input = st.selectbox("Select gender", ["Female", "Male"], index=0)
 
 if __name__ == '__main__':
     main()
