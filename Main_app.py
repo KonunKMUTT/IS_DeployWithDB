c55de8463d1f0612b709c41ec88a43d35ba9dcf9
KonunKMUTT <156557056+KonunKMUTT@users.noreply.github.com>
2024-02-12 14:51:25 +0700

    Main_app.py

--- Main_app.py
+++ Main_app.py
@@ -98,11 +98,6 @@ def main():
         sizes = [female_pct, male_pct]
         colors = ["lightblue", "lightgreen"]
 
-        plt.figure(figsize=(6, 6))
-        plt.pie(sizes, labels=labels, autopct="%1.1f%%", startangle=90, colors=colors)
-        plt.title("Class Distribution")
-        plt.axis("equal")  # Equal aspect ratio for a circular pie chart
-
         st.pyplot()
 
         
